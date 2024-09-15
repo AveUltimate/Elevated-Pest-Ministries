481c08d6b3c0eb1ff2658c977e6e290ebf914515
AveUltimate <59117744+AveUltimate@users.noreply.github.com>
2024-09-15 14:10:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const about_button = document.getElementById("about_button");
 about_button.addEventListener("click", function(e){
     console.log("Someone clicked About us");
-    window.location.href = "https://aveultimate.github.io/Elevated-Pest-Ministries/about.html";
+    window.location.href = "about.html";
 }); //listens for click
 
 const preach_button = document.getElementById("preach_button");
 preach_button.addEventListener("click",function(){
-    window.location.href = "https://aveultimate.github.io/Elevated-Pest-Ministries/preach_info.html";
+    window.location.href = "preach_info.html";
 })
